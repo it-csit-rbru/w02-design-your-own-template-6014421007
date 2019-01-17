85dddddfcb609511a052a26db7f2ced09fc2dd6b
Nantawat Nakkham <60014421007@rbru.ac.th>
2019-01-17 23:02:41 +0700

    I

---
+++ includes/connect_db.php
@@ -0,0 +1,20 @@
+<?php
+
+//กำหนดตั้งค่า database ทั้งหมด
+$configs = [
+    "host" => "localhost",
+    "username" => "root",
+    "password" => "root",
+    "db_name" => "php-pro"
+];
+// ทำการ connect database
+$connect = mysqli_connect( 
+    $configs["host"], 
+    $configs["username"], 
+    $configs["password"],
+    $configs["db_name"]
+);
+// ทำการ check login
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
---
+++ login/login_check.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ *  check ค่า email ว่าเป็นค่า ว่าง หรือป่าว
+ */
+
+if(isset($_POST['email'])){
+
+    //เก็บค่าตัวแปล email ที่ส่งมาจาก form ใส่ในตัวแปร $email
+    $email = $_POST['email'];
+
+    /**
+     * check ค่า password กับ email ว่าเป็นค่า ว่าง หรือป่าว
+     */
+
+    if(isset($_POST['password']) && $email != ''){
+
+        //เก็บค่าตัวแปล password ที่ส่งมาจาก form ใส่ในตัวแปร $password
+        $password = $_POST['password'];
+
+        /**
+         * ทำการ require connect database
+         */
+
+        require '../includes/connect_db.php';
+
+        /**
+         * นำ $email กับ $password มาเข้า 
+         * function : > mysqli_real_escape_string > strip_tags 
+         */
+
+        $email = strip_tags(mysqli_real_escape_string($connect, trim($email)));
+        $password = strip_tags(mysqli_real_escape_string($connect, trim($password)));
+    
+        /**
+         * ทำการ SELECT Database จาก ฐานข้อมูล
+         */
+
+        $login_query = mysqli_query($connect, "SELECT * FROM users WHERE email='$email'");
+
+            /**
+             * mysqli_num_rows($login_query)>0
+             * นำค่าที่ query จาก $login_query ได้มาทำการนับ row ใน ฐานข้อมูล
+             */
+
+            if(mysqli_num_rows($login_query) > 0){
+
+                //นำ $login_query มาทำเป็น array
+                $login_row = mysqli_fetch_array($login_query);
+
+                //นำ password ที่ query ออกมา มาใส่ในตัวแปร $password_hash
+                $password_hash = $login_row['password'];
+
+                /**
+                 * ทำการ check password 
+                 * นำค่าที่ user กรอกผ password ผ่าน form มาตรวจสอบรหัส
+                 */
+
+                if(password_verify($password,$password_hash)){
+
+                    /**
+                     * ถ้า password ตรงกับ user ที่กรอกเข้ามา
+                     * จะเข้า เงื่อนไข if
+                     */
+                    session_start();
+                    $smg = "Login สำเสร็จ!";
+                    $login_status->id = $login_row['id'];
+                    $login_status->email = $login_row['email'];
+                    $login_status->name = $login_row['firstname'];
+                    $_SESSION['login'] = json_encode($login_status);
+                    header('Location: http://localhost/php-project/main.php');
+                    exit();
+                }else{
+
+                    /**
+                     * ถ้า password ไม่ถูกต้องจะทำการส่งค่า $smg เป็น string 
+                     * และทำการ exit(); เพื่อหยุดการทำงาน
+                     */
+
+                    $smg = "Login ล้มเหลว!";
+                    header('Location: http://localhost/php-project/login/');
+                    exit();
+                }
+            }else{
+                echo '<h1>อีเมลไม่ถูกต้อง</h1>';
+                exit();
+            }
+    }else{
+        echo '<h1>ไม่ได้กรอก email หรือ password</h1>';
+        exit();
+    }
+}else{
+    echo '<h1>ไม่ได้กรอก email</h1>';
+    exit();
+}
+
+
